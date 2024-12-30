68a96d81636dfcb91dd26b0b1987ccae825cd8b5
Tommaso Ramella <tommaso.ramella90@gmail.com>
2024-12-30 21:45:31 +0100

    Vercel

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tommella-frontend/' : '/',
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: 'public/_redirects', to: '' },
-        ],
-      }),
-    ],
-  },
-};
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ? '/tommella-frontend/' : '/',
+//   configureWebpack: {
+//     plugins: [
+//       new CopyWebpackPlugin({
+//         patterns: [
+//           { from: 'public/_redirects', to: '' },
+//         ],
+//       }),
+//     ],
+//   },
+// };
